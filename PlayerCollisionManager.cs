ab72edd3337609a88c12250e4c0f384f8146afb5
Anvi Penmetsa <91691176+anvi-pen@users.noreply.github.com>
2023-09-21 14:43:43 -0700

    Add files via upload

---
+++ PlayerCollisionManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+This script manages the effect that occurs if the player walks into
+a water vapor game object, which would be to increase the player's
+suspicion level.
+*/
+
+public class PlayerCollisionManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "WaterVapor")
+        {
+            gameObject.GetComponent<PlayerSusManager>().incrementSus();
+        }
+    }
+}
